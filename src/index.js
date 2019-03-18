8794a1670213245f9840a3c8b7f70f7c9f4a4d88
scepanenka <scepanenka@gmail.com>
2019-03-18 22:53:02 +0300

    reafactoring

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 module.exports = function makeExchange(currency) {
-    if (currency > 10000) {
-        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
-    }
+  if (currency > 10000) {
+    return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+  }
 
-    let coins = {};
+  let coins = {};
 
-    if (currency / 50 >= 1) {
-        coins.H = Math.floor(currency / 50);
-        currency = currency % 50;
-    }
+  if (currency >= 50) {
+    coins.H = Math.trunc(currency / 50);
+    currency = currency % 50;
+  }
 
-    if (currency / 25 >= 1) {
-        coins.Q = Math.floor(currency / 25);
-        currency = currency % 25;
-    }
+  if (currency >= 25) {
+    coins.Q = Math.trunc(currency / 25);
+    currency = currency % 25;
+  }
 
-    if (currency / 10 >= 1) {
-        coins.D = Math.floor(currency / 10);
-        currency = currency % 10;
-    }
+  if (currency >= 10) {
+    coins.D = Math.trunc(currency / 10);
+    currency = currency % 10;
+  }
 
-    if (currency / 5 >= 1) {
-        coins.N = Math.floor(currency / 5);
-        currency = currency % 5;
-    }
+  if (currency >= 5) {
+    coins.N = Math.trunc(currency / 5);
+    currency = currency % 5;
+  }
 
-    if (currency >= 1) {
-        coins.P = currency;
-    }
+  if (currency >= 1) {
+    coins.P = currency;
+  }
 
-    return coins;
+  return coins;
 }
